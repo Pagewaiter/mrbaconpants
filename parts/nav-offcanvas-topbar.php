973cce8b26230ce09d182b1eccc74b2ed00a3cfd
Jason Mosley <jmosley@pagewaiter.com>
2016-05-16 14:56:49 -0400

    updated starter theme.

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -4,7 +4,7 @@
 <div class="top-bar" id="top-bar-menu">
 	<div class="top-bar-left float-left">
 		<ul class="menu">
-			<li class="menu-text"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
+			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
 		</ul>
 	</div>
 	<div class="top-bar-right show-for-medium">
@@ -13,7 +13,7 @@
 	<div class="top-bar-right float-right show-for-small-only">
 		<ul class="menu">
 			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas">Menu</a></li>
+			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
 		</ul>
 	</div>
 </div>
--- parts/nav-title-bar.php
+++ parts/nav-title-bar.php
@@ -4,13 +4,13 @@ $breakpoint = "medium"; ?>
 
 <div class="title-bar" data-responsive-toggle="top-bar-menu" data-hide-for="<?php echo $breakpoint ?>">
   <button class="menu-icon" type="button" data-toggle></button>
-  <div class="title-bar-title">Menu</div>
+  <div class="title-bar-title"><?php _e( 'Menu', 'jointswp' ); ?></div>
 </div>
 
 <div class="top-bar" id="top-bar-menu">
 	<div class="top-bar-left show-for-<?php echo $breakpoint ?>">
 		<ul class="menu">
-			<li class="menu-text"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
+			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
 		</ul>
 	</div>
 	<div class="top-bar-right">
