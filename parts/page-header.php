4c1c689abf6691bc4b46ff3f81914b7ded4ceecd
Jason Mosley <jmosley@pagewaiter.com>
2016-05-20 13:19:13 -0400

    added header image and place holders

---
+++ parts/page-header.php
@@ -0,0 +1,13 @@
+<section id="page-header">
+	<div class="row">
+        
+        <div id="page-logo" class="large-4 medium-4 columns text-center">
+				<img src="http://s3-us-west-2.amazonaws.com/mrbaconpants/wp-content/uploads/2013/06/sm-bacon-300x300.png"  alt="logo" />					    					
+	    </div> <!-- end #page-logo -->
+        
+        <div id="page-embed" class="large-8 medium-8 columns">
+				Podcast Player					    					
+	    </div> <!-- end #page-embed -->
+        
+    </div> <!-- end row -->
+</section>
