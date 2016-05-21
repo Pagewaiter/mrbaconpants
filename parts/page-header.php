fe046ccf1088bac2793590e68d390eb791b4f3bd
Jason Mosley <jmosley@pagewaiter.com>
2016-05-21 00:09:27 -0400

    fixed babel and npm package file also updated the page harder a little bit.

--- parts/page-header.php
+++ parts/page-header.php
@@ -2,11 +2,12 @@
 	<div class="row">
         
      	<div id="page-logo" class="large-4 medium-4 columns text-center">
-				<img src="http://s3-us-west-2.amazonaws.com/mrbaconpants/wp-content/uploads/2013/06/sm-bacon-300x300.png"  alt="logo" />					    					
+            <img src="http://s3-us-west-2.amazonaws.com/mrbaconpants/wp-content/uploads/2013/06/sm-bacon-300x300.png"  alt="logo" />
 	    </div> <!-- end #page-logo -->
         
-      <div id="page-embed" class="large-8 medium-8 columns">
-				<h2>Podcast Player</h2>					    					
+      	<div id="page-embed" class="large-8 medium-8 columns">
+            <h2>Section Title</h2>
+			<aside>Podcast Player</aside>
 	   	</div> <!-- end #page-embed -->
         
     </div> <!-- end row -->
