a3c5d3a42248caa8bfcd907a06fd001478840c44
Jason Mosley <jmosley@pagewaiter.com>
2016-05-21 00:33:10 -0400

    cat conditional

--- parts/page-header.php
+++ parts/page-header.php
@@ -1,8 +1,14 @@
 <section id="page-header">
 	<div class="row">
         
-     	<div id="page-logo" class="large-4 medium-4 columns text-center">
-            <img src="http://s3-us-west-2.amazonaws.com/mrbaconpants/wp-content/uploads/2013/06/sm-bacon-300x300.png"  alt="logo" />
+     	<div id="page-logo" class="large-4 medium-4 columns">
+
+            <?php if ( is_category() ) {
+                echo "category";
+            } else {
+                echo "<img src='http://s3-us-west-2.amazonaws.com/mrbaconpants/wp-content/uploads/2013/06/sm-bacon-300x300.png'  alt='logo' />";
+            }?>
+
 	    </div> <!-- end #page-logo -->
         
       	<div id="page-embed" class="large-8 medium-8 columns">
